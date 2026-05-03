514608747baf8ad17de140cd27f1bcef6ff4611f
Suprema UX <70323230+supremaux@users.noreply.github.com>
2026-05-03 08:43:34 -0400

    iniciamdo o pwa

---
+++ pwabuilder-adv-sw.js
@@ -0,0 +1,3 @@
+
+  import { precacheAndRoute } from 'workbox-precaching/precacheAndRoute';
+  precacheAndRoute([{"revision":"8224ed608a04150bbc37aa381b5d17ae","url":"index.html"},{"revision":"09113d8351256e6cfb426c447f9d8671","url":"main.js"},{"revision":"48a22e7525d81d273c6f5259e0ee5bbb","url":"style.css"}]);
